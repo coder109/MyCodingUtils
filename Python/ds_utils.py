055da11d5836a4938033146112cccc276aa59dc9
Coder109 <3211697682@qq.com>
2025-07-03 15:41:01 +0800

    Update ds_utils.py: Update remove_chinese_and_japanese_chars

--- Python/ds_utils.py
+++ Python/ds_utils.py
@@ -2,6 +2,8 @@
 This module provides utility functions for working with Data Structures.
 """
 
+import re
+
 def remove_empty_str(lst):
     """
     Removes empty strings from a list of strings.
@@ -44,3 +46,16 @@ def remove_empty_char_in_lst(lst):
     for e in lst:
         result.append(e.strip())
     return result
+
+def remove_chinese_and_japanese_chars(text):
+    """
+    Removes Chinese and Japanese characters from a string.
+
+    Args:
+        text (str): string
+
+    Returns:
+        str: string with Chinese and Japanese characters removed
+    """
+    pattern = re.compile(r'[\u3040-\u30FF\u4E00-\u9FFF]')
+    return pattern.findall(text)
